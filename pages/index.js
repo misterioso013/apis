fe05d1f22380af24ae9dd14275f3af0413adabfd
Rosiel Victor <codepremium917@gmail.com>
2021-12-26 08:49:54 -0300

    Title

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Head from "next/head"
 export default function Home() {
   return (
    <div>
+     <Head>
+       <title>Misterioso013 - APIs</title>
+     </Head>
      <h1>Minhas APIs</h1>
-     <h2><a href="">Veja a Documentação</a></h2>
+     <h2><a href="https://github.com/misterioso013/apis">Veja a Documentação</a></h2>
      <p>
        Olá, eu me chamo Rosiel Victor e aqui está hospedado as minhas APIs.<br/>
        Você poderá utilizar sem qualquer restrição, mas se precisar de um número muito grande de requisições, recomendo fazer o Deploy do código fonte em sua própria conta da Vercel!
